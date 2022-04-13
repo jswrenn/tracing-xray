62411d0957ea2c1ec5400e2f4516cf77240344cd
Bryan Garza <1396101+bryangarza@users.noreply.github.com>
2022-04-13 21:16:28 +0000

    Define a DaemonClient to talk to X-Ray over UDP
    This patch adds a `DaemonClient` type that uses the typestate pattern
    to track the connection status of the `UdpSocket`. Once connected, we
    are able to make use of the `send(...)` method. This includes the
    daemon header automatically.
    
    See "Sending segment documents to the X-Ray daemon":
    https://docs.aws.amazon.com/xray/latest/devguide/xray-api-sendingdata.html

---
+++ src/xray_daemon.rs
@@ -0,0 +1,64 @@
+use std::io;
+use tokio::net::UdpSocket;
+
+const DAEMON_HEADER: &[u8] = b"{\"format\": \"json\", \"version\": 1}\n";
+const DEFAULT_UDP_REMOTE_PORT: u16 = 2000;
+
+pub(crate) struct DaemonClient<S: ClientState> {
+    state: S,
+}
+
+struct Start {
+    remote_port: u16,
+}
+struct Connected {
+    sock: UdpSocket,
+}
+
+pub(crate) trait ClientState {}
+impl ClientState for Start {}
+impl ClientState for Connected {}
+
+impl DaemonClient<Start> {
+    pub(crate) fn new(remote_port: u16) -> Self {
+        DaemonClient {
+            state: Start { remote_port },
+        }
+    }
+
+    pub(crate) async fn connect(&self) -> io::Result<DaemonClient<Connected>> {
+        // Let the OS choose an IP and port for us...
+        let sock = UdpSocket::bind("0.0.0.0:0").await?;
+        let remote_addr = format!("127.0.0.1:{}", self.state.remote_port);
+        sock.connect(remote_addr).await?;
+        Ok(DaemonClient {
+            state: Connected { sock },
+        })
+    }
+}
+
+impl Default for DaemonClient<Start> {
+    fn default() -> Self {
+        DaemonClient::new(DEFAULT_UDP_REMOTE_PORT)
+    }
+}
+
+impl DaemonClient<Connected> {
+    pub(crate) async fn send(&self, buf: &[u8]) -> io::Result<usize> {
+        self.state.sock.send(&[DAEMON_HEADER, buf].concat()).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn send_data() {
+        let buf = b"{\"hello\": \"world\"}";
+        let client: DaemonClient<Start> = Default::default();
+        let client = client.connect().await.unwrap();
+        let len = client.send(buf).await.unwrap();
+        assert_eq!(len, DAEMON_HEADER.len() + buf.len());
+    }
+}
